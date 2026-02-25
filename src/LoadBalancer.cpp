2bad1f1c5014252ede7f23e169150f6ca9c10379
n2rowc <n2rowc@tamu.edu>
2026-02-25 12:55:55 -0600

    Load Balancer logic, ready to start coding main

--- src/LoadBalancer.cpp
+++ src/LoadBalancer.cpp
@@ -1,9 +1,14 @@
 #include "LoadBalancer.h"
+#include <cstdlib>
+using namespace std;
 
 LoadBalancer::LoadBalancer(int initialServers,
                            int minQueueMultiplier,
                            int maxQueueMultiplier,
-                           int scaleCooldown)
+                           int scaleCooldown,
+                           int minProcessTime,
+                           int maxProcessTime,
+                           double requestProbability)
     : request_queue_(),
       servers_(),
       ip_blocker_(),
@@ -12,56 +17,115 @@ LoadBalancer::LoadBalancer(int initialServers,
       max_queue_multiplier_{maxQueueMultiplier},
       scale_cooldown_{scaleCooldown},
       last_scale_time_{0},
+      min_process_time_{minProcessTime},
+      max_process_time_{maxProcessTime},
+      request_probability_{requestProbability},
       stats_{} {
-    // TODO
+    for (int i = 0; i < initialServers; i++) {
+        servers_.push_back(new WebServer(i));
+    }
 }
 
 void LoadBalancer::addServer() {
-    // TODO
+    servers_.push_back(new WebServer(servers_.size()));
+    servers_added_++;
 }
 
 void LoadBalancer::removeServer() {
-    // TODO
+    if (!servers_.empty()) {
+        delete servers_.back();
+        servers_.pop_back();
+        servers_removed_++;
+    }
 }
 
 void LoadBalancer::runCycle() {
-    // TODO
+    current_time_++;
+    if (request_probability_ > 0.0 && (rand() / static_cast<double>(RAND_MAX)) < request_probability_) {
+        Request r = generateRandomRequest();
+        if (!ip_blocker_.isBlocked(r.ip_in)) {
+            request_queue_.enqueue(r);
+            total_requests_generated_++;
+        } else {
+            total_requests_blocked_++;
+        }
+    }
+    assignRequests();
+    checkScaling();
+    if (request_queue_.size() > static_cast<size_t>(peak_queue_size_)) {
+        peak_queue_size_ = static_cast<int>(request_queue_.size());
+    }
 }
 
 void LoadBalancer::generateInitialQueue() {
-    // TODO
+    for (int i = 0; i < servers_.size() * 100; i++) {
+        request_queue_.enqueue(generateRandomRequest());
+    }
 }
 
 LoadBalancerStats LoadBalancer::getStats() const {
-    // TODO
+    stats_.current_time = current_time_;
+    stats_.total_requests_generated = total_requests_generated_;
+    stats_.total_requests_processed = total_requests_processed_;
+    stats_.total_requests_blocked = total_requests_blocked_;
+    stats_.peak_queue_size = peak_queue_size_;
+    stats_.servers_added = servers_added_;
+    stats_.servers_removed = servers_removed_;
     return stats_;
 }
 
 RequestQueue& LoadBalancer::getQueue() {
-    // TODO
+    RequestQueue& request_queue = request_queue_;
     return request_queue_;
 }
 
+IPBlocker& LoadBalancer::getBlocker() {
+    return ip_blocker_;
+}
+
 int LoadBalancer::getServerCount() const {
-    // TODO
     return static_cast<int>(servers_.size());
 }
 
 int LoadBalancer::getCurrentTime() const {
-    // TODO
     return current_time_;
 }
 
 void LoadBalancer::assignRequests() {
-    // TODO
+    for (int i = 0; i < servers_.size(); i++) {
+        if (servers_[i]->isAvailable()) {
+            servers_[i]->assignRequest(request_queue_.dequeue());
+        }
+    }
 }
 
 void LoadBalancer::checkScaling() {
-    // TODO
+    if (request_queue_.size() < servers_.size() * min_queue_multiplier_) {
+        addServer();
+    }
+    else if (request_queue_.size() > servers_.size() * max_queue_multiplier_) {
+        removeServer();
+    }
 }
 
 Request LoadBalancer::generateRandomRequest() const {
-    // TODO
-    return Request{};
+    Request request;
+    int ip_c = rand() % 256;
+    int ip_d = rand() % 256;
+    request.ip_in = "192.168." + to_string(ip_c) + "." + to_string(ip_d);
+
+    int ip2_b = rand() % 256;
+    int ip2_c = rand() % 256;
+    int ip2_d = rand() % 256;
+    request.ip_out = "10." + to_string(ip2_b) + "." +
+                     to_string(ip2_c) + "." + to_string(ip2_d);
+
+    int range = max_process_time_ - min_process_time_ + 1;
+    request.processing_time = min_process_time_ + (rand() % range);
+
+    request.job_type = (rand() % 2 == 0) ? 'P' : 'S';
+    request.arrival_time = current_time_;
+
+    return request;
 }
 
--- src/LoadBalancer.h
+++ src/LoadBalancer.h
@@ -8,40 +8,52 @@
 #include "WebServer.h"
 #include "IPBlocker.h"
 
+// Aggregate statistics describing one LoadBalancer run.
 struct LoadBalancerStats {
-    int current_time{};
-    int total_requests_generated{};
-    int total_requests_processed{};
-    int total_requests_blocked{};
-    int peak_queue_size{};
-    int servers_added{};
-    int servers_removed{};
+    int current_time{};             // simulation time in clock cycles
+    int total_requests_generated{}; // how many requests were created
+    int total_requests_processed{}; // how many finished successfully
+    int total_requests_blocked{};   // how many were rejected by IPBlocker
+    int peak_queue_size{};          // largest queue length observed
+    int servers_added{};            // number of times we scaled up
+    int servers_removed{};          // number of times we scaled down
 };
 
+// Manages the request queue, web server pool, and scaling behavior.
 class LoadBalancer {
 public:
+    // Configure initial server count, scaling thresholds, and request-generation config.
     LoadBalancer(int initialServers,
                  int minQueueMultiplier = 50,
                  int maxQueueMultiplier = 80,
-                 int scaleCooldown      = 10);
+                 int scaleCooldown      = 10,
+                 int minProcessTime     = 5,
+                 int maxProcessTime     = 50,
+                 double requestProbability = 0.05);
 
+    // Add or remove a single web server from the pool.
     void addServer();
     void removeServer();
 
+    // Execute one simulation cycle (update servers, queue, and scaling).
     void runCycle();
 
+    // Fill the queue with an initial batch of requests (servers * 100).
     void generateInitialQueue();
 
+    // Access simulation statistics, queue, and IP blocker.
     LoadBalancerStats getStats() const;
     RequestQueue&     getQueue();
+    IPBlocker&        getBlocker();
 
     int getServerCount() const;
     int getCurrentTime() const;
 
 private:
-    void    assignRequests();
-    void    checkScaling();
-    Request generateRandomRequest() const;
+    // Helpers used internally by runCycle().
+    void    assignRequests();          // give queued work to available servers
+    void    checkScaling();           // decide whether to add/remove servers
+    Request generateRandomRequest() const; // build a new random Request
 
 private:
     RequestQueue            request_queue_;
@@ -53,9 +65,20 @@ private:
     int max_queue_multiplier_{80};
     int scale_cooldown_{10};
     int last_scale_time_{0};
+    int min_process_time_{5};
+    int max_process_time_{50};
+    double request_probability_{0.05};
+
+    int total_requests_generated_{0};
+    int total_requests_processed_{0};
+    int total_requests_blocked_{0};
+    int peak_queue_size_{0};
+    int servers_added_{0};
+    int servers_removed_{0};
 
     LoadBalancerStats stats_;
 };
 
-#endif
+#endif // LOADBALANCER_H
+
 
